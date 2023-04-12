ab833ae90d6454869c0163d16ade61532170f977
Abdallah Mostafa <a.mokhtar@entrenchtech.com>
2023-04-12 22:04:38 +0200

    creating publisher file/logic

---
+++ publisher.js
@@ -0,0 +1,19 @@
+/* RabbitMQ */
+const amqp = require("amqplib");
+
+const msg = {number: process.argv[2]}
+connect();
+async function connect() {
+    try {
+        const amqpServer = "amqp server";
+        const connection = await amqp.connect(amqpServer);
+        const channel = await connection.createChannel();
+        await channel.assertQueue("jobs");
+        await channel.sendToQueue("jobs", Buffer.from(JSON.stringify(msg)));
+        console.log(`Job sent successfully ${msg.number}`);
+        await channel.close();
+        await connection.close();
+    } catch (error) {
+        console.error(error)
+    }
+}
