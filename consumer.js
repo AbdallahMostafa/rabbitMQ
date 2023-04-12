561e0a870be464f676ea29e4b292d38083095cdc
Abdallah Mostafa <a.mokhtar@entrenchtech.com>
2023-04-12 22:04:16 +0200

    creating consumer.js file/logic

---
+++ consumer.js
@@ -0,0 +1,22 @@
+const amqp = require("amqplib");
+
+connect();
+
+async function connect(){
+    try {
+        const amqpServer = "amqp server";
+        const connection = await amqp.connect(amqpServer);
+        const channel = await connection.createChannel();
+        await channel.assertQueue("jobs");
+        channel.consume("jobs", message => {
+            const input = JSON.parse(message.content.toString());
+            console.log(`Recived job with input ${input.number}`);
+            // testing ack
+            if (input.number == 7)
+                channel.ack(message);
+        });
+        console.log("waiting for messages...");
+    } catch (error) {
+        console.error(error);
+    }
+}
